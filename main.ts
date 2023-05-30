11122f6233e2300fcd79f1107cb1ff18c195a41f
Los-wapos-xd <131193171+Los-wapos-xd@users.noreply.github.com>
2023-05-30 11:23:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        while (receivedNumber == 2) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        }
     }
     if (receivedNumber == 3) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        while (receivedNumber == 3) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        }
     }
     if (receivedNumber == 4) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 125)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 125)
+        while (receivedNumber == 4) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 125)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 125)
+        }
     }
     if (receivedNumber == 5) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 125)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 125)
+        while (receivedNumber == 5) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 125)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 125)
+        }
     }
 })
 input.onGesture(Gesture.ScreenDown, function () {
     radio.sendNumber(3)
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(5)
-})
 input.onGesture(Gesture.ScreenUp, function () {
     radio.sendNumber(2)
 })
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(4)
 })
-radio.setGroup(1)
-basic.forever(function () {
-	
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(5)
 })
+radio.setGroup(1)
